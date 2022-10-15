f79bcce5af9ea0367d71074ee1400c28853aa8e3
ashishbhatnagarCP <ashishbhatnagar.cp@gmail.com>
2022-10-16 00:06:33 +0530

    creating custom hook

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,29 +2,31 @@ import "./App.css";
 import { useState } from "react";
 // const cities=['New York',"New Delhi","Pune"]
 
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  return [
+    {
+      value,
+      onChange: (e) => setValue(e.target.value),
+    },
+    () => setValue(initialValue),
+  ];
+}
+
 function App() {
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
   const submit = (e) => {
     e.preventDefault();
-    alert(`${title} and ${color}`);
-    setTitle("");
-    setColor("#000000");
+    alert(`${titleProps.value} and ${colorProps.value}`);
+    resetTitle();
+    resetColor();
   };
   return (
     <form onSubmit={submit}>
-      <input
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        type="text"
-        placeholder="color title...."
-      ></input>
-      <input
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
-        type="color"
-      ></input>
+      <input {...titleProps} type="text" placeholder="color title...."></input>
+      <input {...colorProps} type="color"></input>
       <button>Add</button>
     </form>
   );
