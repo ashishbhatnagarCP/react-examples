6bba7a2412c9ff82f0de69c199f511b5fb5bb4b2
ashishbhatnagarCP <ashishbhatnagar.cp@gmail.com>
2022-10-15 23:44:02 +0530

    refactoring with useReducer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
-import { useState } from "react";
+import { useReducer } from "react";
 // const cities=['New York',"New Delhi","Pune"]
 
 function App() {
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
   return (
     <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-      ></input>
-      <label>{checked?"checked":"Not checked"}</label>
+      <input type="checkbox" value={checked} onChange={setChecked}></input>
+      <label>{checked ? "checked" : "Not checked"}</label>
     </div>
   );
 }
