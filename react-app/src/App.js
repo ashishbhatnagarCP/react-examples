eb5cd6c0e137ac5a3739a76adaab4d2f899424ff
ashishbhatnagarCP <ashishbhatnagar.cp@gmail.com>
2022-10-16 00:36:06 +0530

    adding loading and error states

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function GithubUser({name,location}){
-  return(
+function GithubUser({ name, location }) {
+  return (
     <div>
       <h1>{name}</h1>
       <h1>{location}</h1>
@@ -12,15 +12,25 @@ function GithubUser({name,location}){
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
   useEffect(() => {
+    setLoading(true);
     fetch(`https://api.github.com/users/ashishbhatnagarCP`)
       .then((response) => response.json())
-      .then(setData);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
-  if (data) {
-    return <GithubUser name={data.name} location={data.location}/>
+
+  if (loading) {
+    return <h1>Loading ....</h1>;
   }
-  return <h1>Data</h1>;
+  if (error) {
+    return <pre>{JSON.stringify(error)}</pre>;
+  }
+  if (!data) return null;
+  return <GithubUser name={data.name} location={data.location} />;
 }
 
 export default App;
