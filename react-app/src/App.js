98343713356fc0bba0c3abbf857c5ccf49a1c3da
ashishbhatnagarCP <ashishbhatnagar.cp@gmail.com>
2022-10-15 23:41:33 +0530

    Added a checkbox change using state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,45 +1,17 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-
+import { useState } from "react";
 // const cities=['New York',"New Delhi","Pune"]
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondaryEmotion, setsecondaryEmotion] = useState("tired");
-
-  useEffect(() => {
-    console.log(`Its ${emotion} around here`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`Its ${secondaryEmotion} around here`);
-  }, [secondaryEmotion]);
-
+  const [checked, setChecked] = useState(false);
   return (
     <div className="App">
-      <h1>Current emotion: {emotion}</h1>
-      <button
-        onClick={() => {
-          setEmotion("sad");
-        }}
-      >
-        Sad
-      </button>
-      <button
-        onClick={() => {
-          setEmotion("excited");
-        }}
-      >
-        Excited
-      </button>
-      <h1>Current Secondary Emotion is {secondaryEmotion}</h1>
-      <button
-        onClick={() => {
-          setsecondaryEmotion("grateful");
-        }}
-      >
-        Grateful
-      </button>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      ></input>
+      <label>{checked?"checked":"Not checked"}</label>
     </div>
   );
 }
